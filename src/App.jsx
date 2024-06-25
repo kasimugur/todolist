5c5b42a42189b7860cac0e743a530806b10dff89
kasimugur <kasiugur7@gmail.com>
2024-06-25 18:55:33 +0300

    todo updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-
   const [todo, setTodo] = useState('')
-  const [task, setTask] = useState([])
+  const [items, setItems] = useState([])
 
   const addItem = () => {
     if (!todo) {
-      alert("please task add")
+      alert('todo giriniz')
+      return;
     }
-    setTask(todo)
-  
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: todo
+    }
+    setItems((list)=> [...list, item] )
+    setTodo('')
+    console.log(item)
   }
-
-  const todoValue = (e) => {
-    setTodo(e.target.value)
-  }
-
   return (
     <>
-      <div className='App'>
-<input type="text" placeholder='add an Item' onChange={todoValue}/>
-<button className='btn-add' onClick={() => addItem()} >Add</button>
-      </div>
-      <ul>
-        {task}
-      </ul>
+      <input type="text"
+        onChange={e => setTodo(e.target.value)}
+        placeholder='add an todos'
+        value={todo} />
+      <button onClick={addItem}>add</button>
+      {items.map((item)=> <li key={item.id}>{item.value} </li>)}
     </>
   )
 }
 
-export default App
+export default App;
