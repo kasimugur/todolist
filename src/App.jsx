6face609edb1344183a70d3569318fc413755294
kasimugur <kasiugur7@gmail.com>
2024-06-26 07:25:36 +0300

    todo updated 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   const [todo, setTodo] = useState('')
   const [items, setItems] = useState([])
-
   const addItem = () => {
     if (!todo) {
       alert('todo giriniz')
@@ -14,9 +13,22 @@ function App() {
       id: Math.floor(Math.random() * 1000),
       value: todo
     }
-    setItems((list)=> [...list, item] )
+    setItems((list) => [...list, item])
     setTodo('')
-    console.log(item)
+  }
+  useEffect(() => {
+    localStorage.setItem('todos',
+      JSON.stringify(items)
+    )
+  }, [items])
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem('todos'))
+    if (storedTodos) {
+      setItems(storedTodos)
+    }
+  }, [])
+  const removeItem = (id) => {
+    setItems(items.map(e => e.id !== id))
   }
   return (
     <>
@@ -24,8 +36,9 @@ function App() {
         onChange={e => setTodo(e.target.value)}
         placeholder='add an todos'
         value={todo} />
-      <button onClick={addItem}>add</button>
-      {items.map((item)=> <li key={item.id}>{item.value} </li>)}
+      <button onClick={() => addItem()}>add</button>
+      {items.map((item) => <li key={item.id}>{item.value} <button
+        onClick={() => removeItem(items.id)}>X</button> </li>)}
     </>
   )
 }
